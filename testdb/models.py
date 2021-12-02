330f26c74162f97540d8f81d3a47cfcd5160ee29
Brian K. Vagnini <brian@vagnini.net>
2021-12-02 15:07:58 -0500

    Add tutorial url to models.py file.

--- testdb/models.py
+++ testdb/models.py
@@ -4,3 +4,5 @@ class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
     classname = models.CharField(max_length=80)
+
+    #from https://stackpython.medium.com/how-to-start-django-project-with-a-database-postgresql-aaa1d74659d8  
